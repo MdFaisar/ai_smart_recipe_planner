2990046011151369649b9d42093d37824fa0e17d
MdFaisar <amohammedfaisar@gmail.com>
2024-12-26 11:49:11 +0530

    Update app.py

--- src/app.py
+++ src/app.py
@@ -5,6 +5,10 @@ import json
 from pathlib import Path
 import sys
 
+headers ={ 
+   "authorization": st.secrets["groq_api_key"]
+}
+
 # Add the project root to the Python path
 project_root = Path(__file__).resolve().parent.parent
 sys.path.append(str(project_root))
